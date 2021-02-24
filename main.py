7301c38a0e35f714c59f31dc172fb77c06146b50
jakubek0708 <jakubek0708@gmail.com>
2021-02-24 14:25:57 +0100

    zul

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ async def oof(ctx):
 @client.command(aliases=['CBT'])
 async def cbt(ctx):
     guild = ctx.guild
-    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+    voice_client: discord.VoiceClient = discord.utils.get(client.voice_clients, guild=guild)
     audio_source = discord.FFmpegPCMAudio('/opt/zbiorbot/cbt.mp3')
     if not voice_client.is_playing():
         voice_client.play(audio_source, after=None)
