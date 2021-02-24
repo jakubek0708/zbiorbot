6b2d6e6596e6c3aa8e8621e53cb02011f7eb75fb
jakubek0708 <jakubek0708@gmail.com>
2021-02-24 15:01:08 +0100

    jhjgjhg

--- main.py
+++ main.py
@@ -113,7 +113,8 @@ async def oof(ctx):
 async def cbt(ctx):
 
     guild = ctx.guild
-    voice_client: discord.VoiceClient = discord.utils.get(client.voice_clients, guild=guild)
+    discord.VoiceClient = discord.utils.get(client.voice_clients, guild=guild)
+    voice_client = discord.VoiceClient
     audio_source = discord.FFmpegPCMAudio('/opt/zbiorbot/cbt.mp3')
     if not voice_client.is_connected():
         channel = ctx.author.voice.channel
